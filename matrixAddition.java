74ea2d8b81f07bc3dce470d9887aafa6cb0df00e
ssjthegoat <shreshtha.yadav.s.125@kalvium.community>
2025-10-13 17:05:43 +0530

    Renamed prac to matrixAddition

--- matrixAddition.java
+++ matrixAddition.java
@@ -1,5 +1,5 @@
 import java.util.Scanner;
-public class prac {
+public class matrixAddition {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter the size of first matrix: ");
