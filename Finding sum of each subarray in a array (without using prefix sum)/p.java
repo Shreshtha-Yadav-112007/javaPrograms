98903e5ab8bd14bd90e50895d81ec8e14f2abebc
ssjthegoat <shreshtha.yadav.s.125@kalvium.community>
2025-10-15 21:14:25 +0530

    Made a few new programs about subarrays

---
+++ Finding sum of each subarray in a array (without using prefix sum)/p.java
@@ -0,0 +1,14 @@
+public class p {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3};
+        for(int start=0;start<arr.length;start++){
+            for(int end=start;end<arr.length;end++){
+                int sum=0;
+                for(int k=start;k<=end;k++){
+                    sum+=arr[k];
+                }
+                System.out.println("Sum of subarray "+start+" to "+end+" = "+sum);
+            }
+        }
+    }
+}
