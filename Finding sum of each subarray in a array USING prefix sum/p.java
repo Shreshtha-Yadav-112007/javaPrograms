560befc0c9b1d27afce088226d03a58b1e105bfc
ssjthegoat <shreshtha.yadav.s.125@kalvium.community>
2025-10-16 23:37:14 +0530

    Made new programs and learnt more about subarrays and prefix sum

---
+++ Finding sum of each subarray in a array USING prefix sum/p.java
@@ -0,0 +1,26 @@
+public class p {
+    public static void main(String[] args) {
+        int [] arr = {1,2,3};
+        int [] prefix = new int[3];
+        for(int i=0;i<arr.length;i++){
+            if(i==0){
+                prefix[i]=arr[i];
+            }
+            else{
+                prefix[i] = prefix[i-1]+arr[i];
+            }
+        }
+        for(int start = 0;start<prefix.length;start++){
+            for(int end = start;end<prefix.length;end++){
+                    int sum=0;
+                    if(start ==0){
+                        sum = prefix[end];
+                    }
+                    else{
+                        sum = prefix[end]-prefix[start-1];
+                    }
+                    System.out.println(sum);
+            }
+        }
+    }
+}
